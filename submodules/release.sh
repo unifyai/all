8a939792f46d7a5510080f56fe4efbf57c3a5cd3
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-29 11:25:20 +0100

    updated release.sh to assert that git is on the master branch, and that this branch is fully in sync with the upstream branch (the commits are neither ahead nor behind).

--- submodules/release.sh
+++ submodules/release.sh
@@ -9,7 +9,23 @@ release_submodule() {
   printf '\nReleasing %s\n\n' "$SUBMODULE"
 
   if [[ $(git status --porcelain) ]]; then
-    echo "Git changes detected, either add and commit these or remove them before installing, exiting"
+    echo "Git changes detected, either add and commit these or remove them before installing. Exiting"
+    exit
+  fi
+
+  GIT_STATUS_POR_BRA=$(git status --porcelain --branch)
+  if [[ "$GIT_STATUS_POR_BRA" == *"ahead"* ]]; then
+    echo "Local commits are ahead of upstream, push these before releasing. Exiting."
+    exit
+  fi
+
+  if [[ "$GIT_STATUS_POR_BRA" == *"behind"* ]]; then
+    echo "Local commits are behind upstream, pull and/or merge these before releasing. Exiting."
+    exit
+  fi
+
+  if ! [[ "$GIT_STATUS_POR_BRA" == *"## master"* ]]; then
+    echo "Currently not on master branch, move to master branch before releasing. Exiting."
     exit
   fi
 
@@ -23,12 +39,12 @@ release_submodule() {
   fi
 
   if ! grep -Fq "$VERSION_OLD" setup.py; then
-      echo "The old version is not present in setup.py, exiting"
+      echo "The old version is not present in setup.py. Exiting"
       exit
   fi
 
   if grep -Fq "$VERSION_NEW" setup.py; then
-      echo "The new version already exists in setup.py, exiting"
+      echo "The new version already exists in setup.py. Exiting"
       exit
   fi
 
@@ -38,14 +54,14 @@ release_submodule() {
   PIP_HAS_OLD=$(echo "$PIP_RET" | grep -F "$VERSION_OLD")
 
   if [ -z "$PIP_HAS_OLD" ]; then
-      echo "The old version not found in PyPI, exiting"
+      echo "The old version not found in PyPI. Exiting"
       exit
   fi
 
   PIP_HAS_NEW=$(echo "$PIP_RET" | grep -F "$VERSION_NEW")
 
   if [ -n "$PIP_HAS_NEW" ]; then
-      echo "The new version already in PyPI, exiting"
+      echo "The new version already in PyPI. Exiting"
       exit
   fi
 
