ac8c1fb6cf40f5f630c92e8a112f687270d274f4
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-29 20:14:46 +0100

    modified text on release failure to print to terminal in red.

--- submodules/release.sh
+++ submodules/release.sh
@@ -9,61 +9,61 @@ release_submodule() {
   printf '\nReleasing %s\n\n' "$SUBMODULE"
 
   if [[ $(git status --porcelain) ]]; then
-    echo "Git changes detected, either add and commit these or remove them before installing. Exiting"
+    tput setaf 1; echo "Git changes detected, either add and commit these or remove them before installing. Exiting"
     exit
   fi
 
   GIT_STATUS_POR_BRA=$(git status --porcelain --branch)
 
   if [[ "$GIT_STATUS_POR_BRA" == *"ahead"* ]]; then
-    echo "Local commits are ahead of upstream, push these before releasing. Exiting."
+    tput setaf 1; echo "Local commits are ahead of upstream, push these before releasing. Exiting."
     exit
   fi
 
   if [[ "$GIT_STATUS_POR_BRA" == *"behind"* ]]; then
-    echo "Local commits are behind upstream, pull and/or merge these before releasing. Exiting."
+    tput setaf 1; echo "Local commits are behind upstream, pull and/or merge these before releasing. Exiting."
     exit
   fi
 
   if ! [[ "$GIT_STATUS_POR_BRA" == *"## master"* ]]; then
-    echo "Currently not on master branch, move to master branch before releasing. Exiting."
+    tput setaf 1; echo "Currently not on master branch, move to master branch before releasing. Exiting."
     exit
   fi
 
   TEST_LOG=$(curl -L "https://img.shields.io/github/workflow/status/ivy-dl/$SUBMODULE/nightly-tests")
 
   if [ -z "$TEST_LOG" ]; then
-    echo "The test log returned empty, so cannot determine if the tests are passing or failing. Exiting."
+    tput setaf 1; echo "The test log returned empty, so cannot determine if the tests are passing or failing. Exiting."
     exit
   fi
 
   if [[ "$TEST_LOG" == *"failing"* ]]; then
-    echo "The tests for the latest commit are failing, fix these failing tests before releasing. Exiting."
+    tput setaf 1; echo "The tests for the latest commit are failing, fix these failing tests before releasing. Exiting."
     exit
   fi
 
   if ! [[ "$TEST_LOG" == *"passing"* ]]; then
-    echo "The tests for the latest commit are not passing, fix these failing tests before releasing. Exiting."
+    tput setaf 1; echo "The tests for the latest commit are not passing, fix these failing tests before releasing. Exiting."
     exit
   fi
 
   if [ -z "$VERSION_OLD" ]; then
-      echo "You need to provide an old version number"
+      tput setaf 1; echo "You need to provide an old version number"
       exit
   fi
 
   if [ -z "$VERSION_NEW" ]; then
-      echo "You need to provide a release version number"
+      tput setaf 1; echo "You need to provide a release version number"
       exit
   fi
 
   if ! grep -Fq "$VERSION_OLD" setup.py; then
-      echo "The old version is not present in setup.py. Exiting"
+      tput setaf 1; echo "The old version is not present in setup.py. Exiting"
       exit
   fi
 
   if grep -Fq "$VERSION_NEW" setup.py; then
-      echo "The new version already exists in setup.py. Exiting"
+      tput setaf 1; echo "The new version already exists in setup.py. Exiting"
       exit
   fi
 
@@ -73,14 +73,14 @@ release_submodule() {
   PIP_HAS_OLD=$(echo "$PIP_RET" | grep -F "$VERSION_OLD")
 
   if [ -z "$PIP_HAS_OLD" ]; then
-      echo "The old version not found in PyPI. Exiting"
+      tput setaf 1; echo "The old version not found in PyPI. Exiting"
       exit
   fi
 
   PIP_HAS_NEW=$(echo "$PIP_RET" | grep -F "$VERSION_NEW")
 
   if [ -n "$PIP_HAS_NEW" ]; then
-      echo "The new version already in PyPI. Exiting"
+      tput setaf 1; echo "The new version already in PyPI. Exiting"
       exit
   fi
 
