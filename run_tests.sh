4509a3e60f01879056eba846f9739660d5f2adc5
JG <67888047+juliagsy@users.noreply.github.com>
2022-08-16 20:41:33 +0800

    Update run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-docker run --rm -it -v "$(pwd)":/ivy_all unifyai/ivy-all:latest python3 -m pytest all_tests_n_demos/
+docker run --rm -it -v "$(pwd)":/ivy_all unifyai/all:latest python3 -m pytest all_tests_n_demos/
